2360359e3d09c02102ca02b637d6c3ac17589454
Nick <nickp2287@gmail.com>
2020-01-28 21:09:46 +1000

    page transitions implemented with react-spring

--- reacttemplate/src/App.js
+++ reacttemplate/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, useLocation } from "react-router-dom";
+import { useTransition, animated } from "react-spring";
 import Home from "./Home";
 import About from "./About";
 import NavBar from "./NavBar";
@@ -8,26 +9,37 @@ import Main from "./Main";
 import FourZeroFour from "./FourZeroFour";
 
 function App() {
+  //Route Transitions with spring-react
+  const location = useLocation();
+  const transitions = useTransition(location, location => location.pathname, {
+    from: { opacity: "0" },
+    enter: { opacity: "1" }
+  });
+
   return (
     <div>
       <h3 className="appTemplateTitle">React Template</h3>
 
       <NavBar />
 
-      <Switch>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/main">
-          <Main />
-        </Route>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="*">
-          <FourZeroFour />
-        </Route>
-      </Switch>
+      {transitions.map(({ item, props, key }) => (
+        <animated.div key={key} style={props}>
+          <Switch location={item}>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/main">
+              <Main />
+            </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="*">
+              <FourZeroFour />
+            </Route>
+          </Switch>
+        </animated.div>
+      ))}
     </div>
   );
 }
