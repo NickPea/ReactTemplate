f2d5eca96cc345c21d35a2cdbc4eaee9451e9f37
Nick <nickp2287@gmail.com>
2020-02-01 09:15:55 +1000

    addes some style to the rvsp form and an attend or decline toggle that changes the content of the form

--- reacttemplate/src/components/rsvp.js
+++ reacttemplate/src/components/rsvp.js
@@ -1,47 +1,77 @@
 import React, { useState } from "react";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Card, Nav } from "react-bootstrap";
 
 function RSVP() {
   const [isHasDietReq, setisHasDietReq] = useState(false);
-
   const handleChecked = e => setisHasDietReq(e.target.checked);
-
+  const [activeKey, setactiveKey] = useState("attend");
+  const handleSelect = e => setactiveKey(e);
   return (
     <>
-      <Form className='d-flex flex-column'>
-        <Form.Group>
-          <Form.Label>Name(s)</Form.Label>
-          <Form.Control type="text" />
-        </Form.Group>
+      <Card>
+        <Card.Header className="d-flex font-italic ">
+          <h2>RSVP</h2>
+          <Nav
+            variant="tabs"
+            activeKey={activeKey}
+            className="ml-auto align-self-end"
+            onSelect={handleSelect}>
+            <Nav.Item>
+              <Nav.Link eventKey="attend">Attend</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="decline">Decline</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Card.Header>
+        <Card.Body>
+          <Form className="d-flex flex-column">
+            <Form.Group>
+              <Form.Label>Name(s)</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
 
-        <Form.Group>
-          <Form.Label>Phone Number(s)</Form.Label>
-          <Form.Control type="text" />
-        </Form.Group>
+            {activeKey === "attend" ? (
+              <Form.Group>
+                <Form.Label>Phone Number(s)</Form.Label>
+                <Form.Control type="text" />
+              </Form.Group>
+            ) : null}
 
-        <Form.Group>
-          <Form.Label>Leave Us A Message...</Form.Label>
-          <Form.Control as="textarea" />
-        </Form.Group>
+            <Form.Group>
+              <Form.Label>Leave Us A Message...</Form.Label>
+              <Form.Control as="textarea" />
+            </Form.Group>
 
-        {/* dietary requirments */}
-        <Form.Group>
-          <Form.Check
-            inline
-            onClick={handleChecked}
-            label="Dietary Requirements"
-          />
-        </Form.Group>
-        {isHasDietReq ? (
-          <Form.Group>
-            <Form.Control as="textarea" placeholder="What do you need?" />
-          </Form.Group>
-        ) : null}
+            {/* dietary requirments--> */}
+            {activeKey === "attend" ? (
+              <Form.Group>
+                <Form.Check
+                  inline
+                  onClick={handleChecked}
+                  label="Dietary Requirements"
+                />
+              </Form.Group>
+            ) : null}
+            {isHasDietReq ? (
+              <Form.Group>
+                <Form.Control as="textarea" placeholder="What do you need?" />
+              </Form.Group>
+            ) : null}
+            {/* <--dietary requirments */}
 
-        <Button className='ml-auto' type="submit">RSVP!</Button>
-      </Form>
+            <Button className="ml-auto" type="submit">
+              RSVP!
+            </Button>
+
+          </Form>
+        </Card.Body>
+      </Card>
     </>
   );
 }
 
 export default RSVP;
+
+//style the card
+
